d3ddef369e6039e6e4f739a48f6641bc32554455
woowang789 <wangjh789@gmail.com>
2022-06-28 14:44:44 +0900

    add GatsbyImage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `woowang-gatsby`,
+    title: `My First Gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from "react"
 import Layout from "../components/layout"
 
-// markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
 };
 
-export default IndexPage
+
+export default IndexPage;
