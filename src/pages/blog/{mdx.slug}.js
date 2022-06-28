9b0460c6c0b4edd6c3857b81f0e25998e2dbc6e1
woowang789 <wangjh789@gmail.com>
2022-06-28 17:54:31 +0900

    finished chap 36

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -5,21 +5,28 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost = ({data}) => {
-    const image = getImage(data.mdx.frontmatter.hero_image)
+    const image =
+    data.mdx.frontmatter.hero_image &&
+    getImage(data.mdx.frontmatter.hero_image);
 
     return (
         <Layout pageTitle={data.mdx.frontmatter.title}>
             <p>{data.mdx.frontmatter.date}</p>
-            <GatsbyImage
-                image={image}
-                alt={data.mdx.frontmatter.hero_image_alt}
-            />
-            <p>
-                Photo Credit:{" "}
-                <a href={data.mdx.frontmatter.hero_image_credit_link}>
-                    {data.mdx.frontmatter.hero_image_credit_text}
-                </a>
-            </p>
+            {image && (
+                <>
+                    <GatsbyImage
+                        image={image}
+                        alt={data.mdx.frontmatter.hero_image_alt}
+                    />
+                    <p>
+                        Photo Credit:{" "}
+                        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+                            {data.mdx.frontmatter.hero_image_credit_text}
+                        </a>
+                    </p>    
+                </>
+            )}
+            
             <MDXRenderer>
                 {data.mdx.body}
             </MDXRenderer>
