def677616a27b245519285d568453363e5a44287
woowang789 <wangjh789@gmail.com>
2022-06-28 10:20:36 +0900

    add test page

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TestPage = () => {
+  return (
+    <main>
+      <title>테스트 </title>
+      <h1>테스트  페이지 입니다.</h1>
+    </main>
+  );
+};
+
+export default TestPage;
